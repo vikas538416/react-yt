9de2383e649920a634f2bcff69f811b317cce1fe
Vikas Kasaudhan <kasaudhanv036@gmail.com>
2026-06-30 10:30:37 +0530

    added basic structure of react + vite app

---
+++ folder_1/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () =>{
+    return(
+        <>
+        <div id="dad">
+            <h1 id="child1">App</h1>
+        </div>
+        <div id="chacha">
+            <h3 id="child2">Vikas</h3>
+        </div>
+        </>
+    )
+}
+
+export default App
